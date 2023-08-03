98d3b7ddb126a9ef2b78aa79997740279c7de937
Basudev <samantabasu.2001@gmail.com>
2023-08-03 23:52:30 +0530

    Set lastMessage which is show in recyclerView of MainActivity

--- app/src/main/java/com/example/whatsappclone/Adapters/UserAdapderInChats.java
+++ app/src/main/java/com/example/whatsappclone/Adapters/UserAdapderInChats.java
@@ -14,11 +14,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.whatsappclone.ChatsDetailsAcitivity;
 import com.example.whatsappclone.Models.Users;
 import com.example.whatsappclone.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-public class UserAdapderInChats extends RecyclerView.Adapter<UserAdapderInChats.ViewHolder>{
+public class UserAdapderInChats extends RecyclerView.Adapter<UserAdapderInChats.ViewHolder> {
 
     ArrayList<Users> list;
     Context context;
@@ -31,7 +36,7 @@ public class UserAdapderInChats extends RecyclerView.Adapter<UserAdapderInChats.
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.user_sample_for_chats,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.user_sample_for_chats, parent, false);
         return new ViewHolder(view);
     }
 
@@ -42,13 +47,34 @@ public class UserAdapderInChats extends RecyclerView.Adapter<UserAdapderInChats.
         Picasso.get().load(users.getProfilePic()).placeholder(R.drawable.user).into(holder.imageView);
         holder.userName.setText(users.getUserName());
 
+        //For last message
+        FirebaseDatabase.getInstance().getReference().child("Chats")
+                .child(FirebaseAuth.getInstance().getUid() + users.getUserId())
+                .orderByChild("timestamp")
+                .limitToLast(1)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.hasChildren()){
+                            for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                                holder.lastMessage.setText(dataSnapshot.child("message").getValue().toString());
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(context, ChatsDetailsAcitivity.class);
-                intent.putExtra("userId",users.getUserId());
-                intent.putExtra("profilePic",users.getProfilePic());
-                intent.putExtra("userName",users.getUserName());
+                intent.putExtra("userId", users.getUserId());
+                intent.putExtra("profilePic", users.getProfilePic());
+                intent.putExtra("userName", users.getUserName());
                 context.startActivity(intent);
             }
         });
@@ -64,6 +90,7 @@ public class UserAdapderInChats extends RecyclerView.Adapter<UserAdapderInChats.
 
         ImageView imageView;
         TextView userName, lastMessage;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
